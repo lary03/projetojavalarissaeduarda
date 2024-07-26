d0599c09ffde7256a2e7246f17776ed9223bc104
lary03 <161025589+lary03@users.noreply.github.com>
2024-07-26 12:09:01 -0300

    d

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let chute = prompt('Escolha um número entre 0 e 100');
 // se chute for igual ao número secreto
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (79)');
+alert('Isso ai! Você descobriu o número secreto (7)');
 } else {
 alert('perdeu bebe :(')
 }
