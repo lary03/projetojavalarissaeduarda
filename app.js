23a1488c1fbac6b10794ecd982c72112c5651666
lary03 <161025589+lary03@users.noreply.github.com>
2024-07-26 12:10:02 -0300

    d

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('bem vindo ao terror da programação');
-let numeroSecreto = 7;
+let numeroSecreto = 79;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 0 e 100');
 
 // se chute for igual ao número secreto
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (7)');
+alert('Isso ai! Você descobriu o número secreto (79)');
 } else {
 alert('perdeu bebe :(')
 }
