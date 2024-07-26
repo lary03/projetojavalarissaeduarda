4902f13021fdefc5d35cd21323a079243d1603f3
lary03 <161025589+lary03@users.noreply.github.com>
2024-07-26 12:05:39 -0300

    d

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-alert('Boas vindas ao jogo do número secreto');
+alert('bem vindo ao terror da programação');
 let numeroSecreto = 5;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 0 e 100');
 
 // se chute for igual ao número secreto
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+alert('Isso ai! Você descobriu o número secreto (79)');
 } else {
-alert('Você errou :(')
+alert('perdeu bebe :(')
 }
